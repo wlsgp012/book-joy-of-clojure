67f1d0d67610be4b0da6f4a42a61e05023037cb7
jinhye <wlsgp012@naver.com>
2019-06-23 15:37:57 +0900

    Init

---
+++ test/book_joy_of_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns book-joy-of-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [book-joy-of-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
